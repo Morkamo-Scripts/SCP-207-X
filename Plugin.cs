c4a72ee0a97021942e409dcb98b9ec3caea43899
Кирилл <131317714+Morkamo@users.noreply.github.com>
2025-11-16 16:02:00 +0900

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace SCP_207_X
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "SCP-207-X";
+        public override string Prefix => Name;
+        public override string Author => "Morkamo";
+        public override Version RequiredExiledVersion => new Version(9, 1, 0);
+        public override Version Version => new Version(0, 0, 1);
+
+        public static Plugin Instance;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
